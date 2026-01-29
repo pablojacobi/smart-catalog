7534df76a1a61d2c3dd1efd4f4f1521890fca209
Pablo Jacobi <pjacobi@creditninja.com>
2026-01-29 02:03:57 -0300

    fix: Skip bootsnap in CI to avoid FrozenError with Devise

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup'
-require 'bootsnap/setup'
+
+# Skip bootsnap in CI to avoid frozen array issues with Devise
+require 'bootsnap/setup' unless ENV['CI']
