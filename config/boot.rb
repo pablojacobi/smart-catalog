0b0422c3adbdf7a2ed6b02a0719b2c37b530298a
Pablo Jacobi <pjacobi@creditninja.com>
2026-01-29 02:06:57 -0300

    fix: Disable eager_load in test environment to avoid FrozenError

--- config/boot.rb
+++ config/boot.rb
@@ -3,6 +3,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup'
-
-# Skip bootsnap in CI to avoid frozen array issues with Devise
-require 'bootsnap/setup' unless ENV['CI']
+require 'bootsnap/setup'
